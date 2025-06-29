e5736771bd878e39c7941c953dc07ba210d0f6c4
Manishika Balamurugan <manishika@Manishikas-MacBook-Air.local>
2025-06-28 22:33:10 -0400

    errors with png

--- game.cpp
+++ game.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-const int SIZE = 5;
+const int SIZE = 10;
 const int CELL_SIZE = 100;
 
 enum Cell {
@@ -31,15 +31,31 @@ class Game {
         bool game_end = false;
         sf::RenderWindow window;
 
+        sf::Texture cat_texture;
+        sf::Sprite cat_sprite;
+
     public:
     Game() 
-    : window(sf::VideoMode(sf::Vector2u(SIZE * CELL_SIZE, SIZE * CELL_SIZE)), "Catch the Cat") {
+    : window(sf::VideoMode(sf::Vector2u(SIZE * CELL_SIZE, SIZE * CELL_SIZE)), "Catch the Cat", cat_sprite(cat_texture)) {
     board = vector<vector<Cell>>(SIZE, vector<Cell>(SIZE, EMPTY));
     cat_spot.row = SIZE / 2;
     cat_spot.col = SIZE / 2;
     board[cat_spot.row][cat_spot.col] = CAT;
     srand(time(nullptr));
-}
+
+    if (!cat_texture.loadFromFile("resources/happy_cat.png")) {
+        std::cerr << "Failed to load cat image\n";
+    }
+    cat_sprite.setTexture(cat_texture);
+
+    sf::Vector2u textureSize = cat_texture.getSize();
+
+    cat_sprite.setScale(sf::Vector2f(
+            float(CELL_SIZE) / cat_texture.getSize().x,
+            float(CELL_SIZE) / cat_texture.getSize().y
+        ));
+    }
+   
 
 
         void print_board(){
@@ -50,17 +66,19 @@ class Game {
                     sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
                     cell.setPosition(sf::Vector2f(j * CELL_SIZE, i * CELL_SIZE));      
 
-                    if (board[i][j] == BLOCK){
+                    if (board[i][j] == BLOCK) {
                         cell.setFillColor(sf::Color::Black);
+                        window.draw(cell);
                     }
                     else if (board[i][j] == CAT) {
-                        cell.setFillColor(sf::Color::Yellow);
+                        cat_sprite.setPosition(sf::Vector2f(j * CELL_SIZE, i * CELL_SIZE));
+                        window.draw(cat_sprite);
                     }
                     else {
                         cell.setFillColor(sf::Color::White);
+                        window.draw(cell);
                     }
-
-                    window.draw(cell);
+                    
             }
         }
         window.display();
@@ -156,17 +174,29 @@ class Game {
                         sf::Vector2i mousePos = sf::Mouse::getPosition(window);
                         player_click(mousePos.x, mousePos.y);
 
+
                         cat_move();
 
-                        if (has_escaped()){
-                            window.clear();
-                
-                            sf::Font font;
+                        sf::Font font;
                 
                             if (!font.openFromFile("arial.ttf")) {
                                 cerr << "Failed to load font.\n";
                                 return;
                             }
+
+                        if (game_end){
+                            window.clear();
+                            sf::Text text(font, "You Win!", 24);
+                            window.draw(text);
+                            window.display();
+                            sf::sleep(sf::seconds(5));
+                            return;
+                        }
+
+                        if (has_escaped()){
+                            window.clear();
+                
+                            
                 
                             sf::Text text(font, "You Lose.", 24);
                             window.draw(text);
